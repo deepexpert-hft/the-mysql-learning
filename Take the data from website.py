459b2d045f4ff3d61997a670363c5121ea1a1086
deepexpert-hft <deepexpert_hft@hotmail.com>
2014-12-08 13:52:23 +0800

    Update Take the data from website.py

--- Take the data from website.py
+++ Take the data from website.py
@@ -9,21 +9,36 @@ import re
 def write_to_mysql():
     try:
         cxn = MySQLdb.connect(host='localhost', user='root', passwd='root',db='python')     #与数据库建立连接
-    except:
-        print "Could not connect to MySQL server."
+    except :
+        print "error!"
         exit( 0 )
 
     cur = cxn.cursor()									    #获取操作游标
-    cur.execute("CREATE TABLE dollar(Local_time VARCHAR(20),USDCNH VARCHAR(8),DINIW  VARCHAR(8))")	#创建一个dollar表，包含记录时间、汇率、美元指数三个参数
-    cur.execute("create table oil(Local_time VARCHAR(20),CONC VARCHAR(20))")		    #创建一个oil表，包含记录时间、原油指数两个参数
+    try:
+        cur.execute("CREATE TABLE dollar(Local_time VARCHAR(20),USDCNH VARCHAR(8),DINIW  VARCHAR(8))")	#创建一个dollar表，包含记录时间、汇率、美元指数三个参数
+    except:
+        print 'table dollar exist!'
+    try:
+        cur.execute("create table oil(Local_time VARCHAR(20),CONC VARCHAR(20))")		    #创建一个oil表，包含记录时间、原油指数两个参数
+    except:
+        print 'table oil exist!'
 
     print "数据写入数据库中..."
     while 1:
         value1 = []
         value2 = []
-        result(value1,value2)								    #调用result函数，获取我们从网页抓取的数据
-        cur.execute("INSERT INTO dollar VALUES(%s, %s,%s)" ,value1)			    #将对应数据传入dollar表中
-        cur.execute("INSERT INTO oil VALUES(%s, %s)" ,value2)				    #将对应数据传入oil表中
+        try :
+        	result(value1,value2)							    #调用result函数，获取我们从网页抓取的数据
+        except:
+            print 'No Values'
+        try:
+            cur.execute("INSERT INTO dollar VALUES(%s, %s,%s)" ,value1)			    #将对应数据传入dollar表中
+        except:
+            print 'write to database fail! '
+        try:
+            cur.execute("INSERT INTO oil VALUES(%s, %s)" ,value2)				    #将对应数据传入oil表中
+        except:
+            print 'write to database fail!'
         cxn.commit()									    #提交
         time.sleep(60*60)								    #隔一个小时采样一次
     cxn.close()										    #关闭数据库连接
@@ -48,6 +63,6 @@ def result(value1,value2):
     value1.append(USDCNY[0].encode('utf-8'))
     value1.append(DINIW[0].encode('utf-8'))
     value2.append(CONC[1].encode('utf-8'))
-    print value1,value2
 
-write_to_mysql()
+if '__name__==__main__':
+    write_to_mysql()
